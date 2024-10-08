ad4fa00120ce2a71b282f943d0540e67f980d65a
sulumor <romuald.patfoort@gmail.com>
2024-10-08 17:38:10 +0200

    update all seeding file

--- app/data/seeding.fakers.sql
+++ app/data/seeding.fakers.sql
@@ -3,24 +3,31 @@ BEGIN;
 TRUNCATE TABLE "user", "trace" RESTART IDENTITY CASCADE;
 
 INSERT INTO "user"(firstname, email, password, is_admin) VALUES 
-('Jared', 'tevbez@meramu.mk', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', false),
-('Essie', 'cifkurul@izawbus.cv', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', false),
-('Mario', 'di@puddiknun.uk', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', true),
-('Etta', 'nawadi@ehve.bo', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', true),
-('Sulumor', 'jojoetromu@gmail.com', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', true),
-('Travis', 'jo@ocociec.jo', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', false);
+('Sulumor', 'jojoetromu@gmail.com', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', true);
+-- ('Jared', 'tevbez@meramu.mk', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', false),
+-- ('Essie', 'cifkurul@izawbus.cv', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', false),
+-- ('Mario', 'di@puddiknun.uk', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', true),
+-- ('Etta', 'nawadi@ehve.bo', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', true),
+-- ('Travis', 'jo@ocociec.jo', '$2b$12$WfNSMH0Yob5/JbuS4t9zYeSz1sZCDVCLlxcLS7fyt1EpNbiqxNaNa', false);
 
-INSERT INTO "trace"(strava_id, strava_hash, start, finish, switch, is_a_loop, distance, elevation, description,image) VALUES
-('3260656048129516876','8.1/49.041/1.929','Paris', 'Saint-Ouen-Sur-Seine', 'Cergy', true, 218, 2756, 'Trace CC vers Cergy modifié pour retourner à Saint Ouen sans passer par Cergy', 'src/assets/Screen 1.png' ),
-('3266397698136738982','7.96/48.732/1.942','Clichy','Maison Laffitte', 'Epernon', true, 180, 1511, 'Boucle allant à Epernon et revenant sur Maison Laffitte', 'src/assets/Screen 2.png' ),
-('3266399134342276188','8.11/48.711/1.942','Clichy','Saint-Nom-la-Bretèche', 'Epernon', true, 169, 1430,'Boucle allant à Epernon et revenant sur Saint-Nom-la-Bretèche','src/assets/Screen 3.png'),
-('3266400069424732252','8.27/48.731/1.988','Clichy','Trappes', 'Epernon', true, 125, 997,'Boucle allant à Epernon et revenant sur Trappes','src/assets/Screen 4.png'),
-('3266400749418058836','8.27/48.731/1.988', 'Clichy','Versailles', 'Epernon', true, 136, 1040,'Boucle allant à Epernon et revenant sur Versailles','src/assets/Screen 5.png'),
-('3266401771559766182','8.57/49.0425/2.1196','Clichy','Clichy', 'Marines', true, 120, 1158, 'Petite boucle allant à Marines', 'src/assets/Screen 6.png'),
-('3266402779273247828', '8.06/49.103/2.075', 'Clichy', 'Clichy', 'Marines', true, 165, 1520, 'Grande boucle allant à Marines', 'src/assets/Screen 7.png'),
-('3266403421163991132', '8.45/49.055/2.464', 'Clichy', 'Clichy', 'Senlis', true, 130, 957, 'Petite boucle allant à Senlis', 'src/assets/Screen 8.png'),
-('3266403983694334044', '8.32/49.07/2.516', 'Clichy', 'Clichy', 'Senlis', true, 167, 1225, 'Grande boucle allant à Senlis', 'src/assets/Screen 9.png'),
-('3266404565748522150', '9.01/48.8684/1.9895', 'Clichy', 'Clichy', 'Septeuil', true, 125, 1048, 'Petite boucle allant à Septeuil', 'src/assets/Screen 10.png'),
-('3266405242680998054', '8.72/48.8886/1.9209', 'Clichy', 'Clichy', 'Septeuil', true, 174, 1637, 'Grande boucle allant à Septeuil', 'src/assets/Screen 11.png');
+INSERT INTO "trace"(title, strava_id, strava_hash, start, finish, switch, is_a_loop, distance, elevation, description,image) VALUES
+("Epernon 180km","3278073778981727430","7.96/48.732/1.942","Clichy","Maison Laffite","Épernon",false,180,1511,"Boucle allant à Epernon et revenant sur Maison Laffitte","cdrdaardlcivn3lu6uqm"),
+("Epernon 170km","3278072985145056506","7.86/48.711/1.942","Clichy", "Saint-Nom-la-Bretèche", "Épernon",false, 168, 1431, "Boucle allant à Epernon et revenant sur Saint-Nom-la-Bretèche", "uvnvy12o3thcklczsd8p"),
+("Epernon 136km","3278072630891959462","8.02/48.731/1.988","Clichy", "Versailles", "Épernon",false, 136, 1039, "Boucle allant à Epernon et revenant sur Versailles", "gznyzvmd8wwfrlvsjqdr"),
+("Epernon 125km","3278058410613236902","8.02/48.731/1.988","Clichy", "Trappes", "Épernon",false, 125, 997, "Boucle allant à Epernon et revenant sur Trappes", "sgzrhwjotru13xqf9jd6"),
+("Asnièroise 100km","3274090060887550118","8.6/49.0257/2.2679","Asnières", "Asnières","Beaumont-sur-Oise",true, 100, 997, "Rallye d'Asnières sur la trace de 100km", "dbtcz7rpllhcosbaij4k"),
+("Asnièroise 76km","3274089813748275862","8.6/49.0257/2.3038","Asnières", "Asnières", "Beaumont-sur-Oise",true, 76, 790, "Rallye d'Asnières sur la trace de 76km", "w8fmfmvr3prp6gcnsnhn"),
+("Asnièroise 52km","3274089454964128934","9.05/48.9948/2.2972","Asnières", "Asnières", "Montsoult",true, 53, 454, "Rallye dÁsnières trace de 53km", "kvou18wjdzzhl60b7zay"),
+("Circuit de Baillet","3273380221925510310","8.93/48.992/2.2972","Clichy", "Clichy", "Baillet",true, 59, 419, "Circuit de Baillet", "vru2ehiuh4sc1plukokb"),
+("Conflans-Marly","3273380046053851302","9.16/48.9138/2.1273","Clichy", "Clichy", "Orgeval",true, 83, 534, "Sortie longue Conflans", "urlumqkudgkgdc13pfsq"),
+("Parcours Conflans-Sainte-Honorine","3273379655774771420","9.95/48.9471/2.1977","Clichy", "Clichy", "Conflans-Sainte-Honorine",true, 58, 227, "Petite sortie Conflans", "k1xjceef96klqedgrzrb"),
+("Vallée de l'Oise","3273379380659923110","8.49/49.0261/2.2802","Clichy", "Clichy", "Persan",true, 85, 715, "Ballade dans la vallée de l'Oise", "pkpqv8nqeab6tmdbguxw"),
+("Marines 120km","3278728906700123164","8.32/49.042/2.12","Clichy", "Clichy", "Marines",true, 120, 1158, "Boucle dans le Vexin allant jusqu'à Marines ", "ih1zvemrwqbmdlcrt3rh"),
+("Marines 165km","3278729770674266140","7.81/49.103/2.075","Clichy", "Clichy", "Marines",true, 165, 1519, "Boucle dans le Vexin allant jusqu`à Marines", "ldl8yq7xzmtlsygiclnm"),
+("Senlis 130km","3278730957875472550","8.2/49.055/2.464","Clichy", "Clichy", "Senlis",true, 130, 957, "Boucle dans la forêt de Chantilly allant jusqu'à Senlis", "kxcld2s91zymahuijlqp"),
+("Senlis 167km","3278731632282386598","8.07/49.07/2.516","Clichy", "Clichy", "Senlis",true, 167, 1225, "Boucle dans la forêt de Chantilly jusqu'à Senlis", "pyyyjviobojwdlfbdky5"),
+("Septeuil 126km","3278732235367107612","8.76/48.8684/1.9895","Clichy", "Clichy", "Septeuil",true, 126, 1048, "Boucle dans l'ouest parisien allant jusqu'à Septeuil", "hgay2iti4leesiq1j2ls"),
+("Septeuil 175km","3278732929385779228","8.47/48.8886/1.9209","Clichy", "Clichy", "Septeuil",true, 174, 1637, "Boucle dans l'ouest parisien allant jusqu'à Septeuil", "mhdr7pbh0xl8mu0liy9u"),
+("Clichy-Le Crotoy 2024","3278733761683209308","6.08/49.567/2.057","Clichy", "Le Crotoy", "Chantilly",false, 219, 1636, "Tracé Paris-Le Crotoy modifié pour partir de Clichy", "bas7mg1g6yn56sez2fq8");
 
 COMMIT;
